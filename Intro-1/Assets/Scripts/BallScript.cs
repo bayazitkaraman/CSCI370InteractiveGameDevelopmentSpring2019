15e1841422bfb2ffc9cb547ce3bee2882bbcba6c
Bayazit Karaman <31379894+bayazitkaraman@users.noreply.github.com>
2019-01-30 12:02:55 -0600

    Camera Project

---
+++ Intro-1/Assets/Scripts/BallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody rbBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //You can change gravity feature whenever it starts
+        //rbBall.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rbBall.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
