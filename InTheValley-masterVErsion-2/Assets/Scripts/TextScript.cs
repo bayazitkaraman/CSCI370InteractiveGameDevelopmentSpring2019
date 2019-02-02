f94743788ecad301c7c6c42358593497c6296398
Bayazit Karaman <31379894+bayazitkaraman@users.noreply.github.com>
2019-02-02 16:02:26 -0600

    Chantal2

---
+++ InTheValley-masterVErsion-2/Assets/Scripts/TextScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour
+{
+    public Vector2 screenPosition = new Vector2(0, 0);
+
+    void Update()
+    {
+        Vector3 tempScreenPosition = screenPosition;
+        tempScreenPosition.z = -Camera.main.transform.position.z;
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(tempScreenPosition);
+        worldPosition.x -= GetComponent<Renderer>().bounds.size.x * tempScreenPosition.x / Screen.width;
+        worldPosition.y += GetComponent<Renderer>().bounds.size.y * (1 - tempScreenPosition.y / Screen.height);
+        transform.position = worldPosition;
+    }
+}
